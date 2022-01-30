c11089342f91e07867ad23d46cb759200fa0e95e
uvish <uvish66@gmail.com>
2022-01-30 13:37:44 +0530

    initial

---
+++ pump.ino
@@ -0,0 +1,99 @@
+
+#include <ESP8266WiFi.h>
+ 
+const char* ssid = "Atul ";
+const char* password = "9793065906";
+ 
+int Pump = 13; // GPIO13---D7 of NodeMCU
+int value = LOW;
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+ 
+  pinMode(Pump, OUTPUT);    //D7 as output
+  digitalWrite(Pump, LOW);  //Deactivate Pump
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address on serial monitor
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");    //URL IP to be typed in mobile/desktop browser
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+ 
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+ 
+  
+  if (request.indexOf("/Pump=ON") != -1)  {
+    digitalWrite(Pump, HIGH);
+    value = HIGH;
+  }
+  if (request.indexOf("/Pump=OFF") != -1)  {
+    digitalWrite(Pump, LOW);
+    value = LOW;
+  }
+ 
+// Set Pump according to the request
+//digitalWrite(Pump, value);
+ 
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+ 
+  client.print("Pump is now: ");
+ 
+  if(value == HIGH) {
+    client.print("On");
+  } else {
+    client.print("Off");
+  }
+  client.println("<br><br>");
+  client.println("<a href=\"/Pump=ON\"\"><button>On </button></a>");
+  client.println("<a href=\"/Pump=OFF\"\"><button>Off </button></a><br />");  
+  client.println("</html>");
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println(""); 
+}
