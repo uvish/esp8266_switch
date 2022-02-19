e73bdb0b3b4c79003aeb073cb2aaf6768d0e8cef
uvish <uvish66@gmail.com>
2022-02-19 15:46:29 +0530

    container fix

--- pump/pump.ino
+++ pump/pump.ino
@@ -80,14 +80,14 @@ void loop() {
   client.println("Content-Type: text/html");
   client.println(""); //  do not forget this one
   client.println("<!DOCTYPE HTML>");
-  client.println("<html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\"> <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin> <link href=\"https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap\" rel=\"stylesheet\"> <title>Pump</title> <style>*{margin:0;padding:0}*,:after,:before{box-sizing:border-box}.container{height:100vh;width:100vw}.half{height:50%;width:100%}.upper{display:flex;justify-content:center;align-items:center}.btn{height:5rem;width:5rem;border:none;border-radius:1rem}.btn_on{margin-bottom:5px;margin-right:5px;box-shadow:5px 5px #888}.btn_off{margin-bottom:10px;margin-right:10px;box-shadow:10px 10px #888}.status_on{display:flex;font-family:Roboto,sans-serif;font-size:12rem;color:#90ee90}.status_off{display:flex;font-family:Roboto,sans-serif;font-size:12rem;color:#f08080}.status_btn{color:#d3d3d3}.lower{background:#d3d3d3;display:flex;justify-content:center;align-items:center}</style></head><body><div class=\"container\">");
+  client.println("<html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\"> <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin> <link href=\"https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap\" rel=\"stylesheet\"> <title>Pump</title> <style>*{margin:0;padding:0}*,:after,:before{box-sizing:border-box}.container{height:100vh;width:100vw;display:flex;flex-direction:column;}.half{height:50%;width:100%}.upper{display:flex;justify-content:center;align-items:center}.btn{height:5rem;width:5rem;border:none;border-radius:1rem}.btn_on{margin-bottom:5px;margin-right:5px;box-shadow:5px 5px #888}.btn_off{margin-bottom:10px;margin-right:10px;box-shadow:10px 10px #888}.status_on{display:flex;font-family:Roboto,sans-serif;font-size:12rem;color:#90ee90}.status_off{display:flex;font-family:Roboto,sans-serif;font-size:12rem;color:#f08080}.status_btn{color:#d3d3d3}.lower{background:#d3d3d3;display:flex;justify-content:center;align-items:center}</style></head><body><div class=\"container\">");
   client.println(" <div class=\"upper half\">");
   if(value == HIGH) {
-    client.print("<h1 class=\"status_on\">ON</h1>");
+    client.print("<h1 class=\"status_on\">ON</h1></div>");
     client.println("<div class=\"lower half\">");
      client.println("<a href=\"/Pump=OFF\"><button class=\"btn btn_off\"><h1 class=\"status_btn\">OFF</h1></button></a>");
   } else {
-    client.print("<h1 class=\"status_off\">OFF</h1>");
+    client.print("<h1 class=\"status_off\">OFF</h1></div>");
      client.println("<div class=\"lower half\">");
     client.println("<a href=\"/Pump=ON\"><button class=\"btn btn_on\"><h1 class=\"status_btn\">ON</h1></button></a>");
   } 
